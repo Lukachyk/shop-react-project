a3ef4a20273bcd66995246d43f39993fbf426aa4
Roman Lukachyk <petrovroman26@icloud.com>
2023-02-21 20:06:07 +0200

    add basket icon and module

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { FaShoppingBag } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
+
   return (
     <header>
       <div>
@@ -10,6 +14,14 @@ export default function Header() {
           <li>Контакты</li>
           <li>Кабинет</li>
         </ul>
+        <FaShoppingBag
+          onClick={() => {
+            setCartOpen((cartOpen = !cartOpen));
+          }}
+          className={`shop-cart-button ${cartOpen && "active"}`}
+          size={"1.1em"}
+        />
+        {cartOpen && <div className="shop-cart"></div>}
       </div>
       <div className="presentation"></div>
     </header>
