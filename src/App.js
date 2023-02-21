531b717311a11c13db4b3c5b6011aeab4f763bc2
Roman Lukachyk <petrovroman26@icloud.com>
2023-02-21 17:30:43 +0200

    add items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
+import React from "react";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: "Kitchen",
+          img: "room.jpg",
+          desc: "lreme fdgg good proff of the name or setting",
+          category: "tables",
+          price: "149.00",
+        },
+        {
+          id: 2,
+          title: "Sofa",
+          img: "sofa.jpg",
+          desc: "lreme fdgg good proff of the name or setting",
+          category: "tables",
+          price: "129.00",
+        },
+        {
+          id: 3,
+          title: "Room",
+          img: "room.jpg",
+          desc: "lreme fdgg good proff of the name or setting",
+          category: "tables",
+          price: "180.00",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
